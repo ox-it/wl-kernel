f06be6b5869c017ee2637387bbec47d3a203a35f
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2009-08-14 17:12:11 +0100

    Add some logging about how refreshing a group went.

--- kernel-impl/src/main/java/org/sakaiproject/authz/impl/DbAuthzGroupService.java
+++ kernel-impl/src/main/java/org/sakaiproject/authz/impl/DbAuthzGroupService.java
@@ -2031,6 +2031,7 @@ public abstract class DbAuthzGroupService extends BaseAuthzGroupService
 		 */
 		public void refreshAuthzGroup(BaseAuthzGroup realm)
 		{
+			M_log.debug("refreshAuthzGroup()");
 			if ((realm == null) || (m_provider == null)) return;
 
 			String sql = "";
@@ -2178,6 +2179,9 @@ public abstract class DbAuthzGroupService extends BaseAuthzGroupService
 
 					m_sql.dbWrite(sql, fields);
 				}
+			}
+			if (M_log.isDebugEnabled()) {
+				M_log.debug("refreshAuthzGroup(): deleted: "+ toDelete.size()+ " inserted: "+ toInsert.size()+ " provided: "+ existing.size()+ " nonProvider: "+ nonProvider.size());
 			}
 		}
 
