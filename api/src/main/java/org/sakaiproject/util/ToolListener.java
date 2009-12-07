90f1a2bbb3a088a8d859f70f7e3a2d12c8319e84
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2009-12-07 15:04:12 +0000

    KNL-787 Allow tools folder to be configured. Log when things can't be found.

--- api/src/main/java/org/sakaiproject/util/ToolListener.java
+++ api/src/main/java/org/sakaiproject/util/ToolListener.java
@@ -25,6 +25,7 @@ import java.util.Iterator;
 import java.util.Set;
 import java.io.File;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -42,6 +43,14 @@ import org.sakaiproject.tool.cover.ActiveToolManager;
  * have have a name of the form [toolId][_][localCode].properties.  This format allows
  * tool litles (etc) to be localized even when multiple tool registrations are included.
  * </p>
+ * This listener can be added to the web.xml file with a snippet like this:
+ * <p>
+ * <code>
+ * &lt;listener&gt;<br/>
+ * &nbsp;&lt;listener-class&gt;org.sakaiproject.util.ToolListener&lt;/listener-class&gt;<br/>
+ * &lt;/listener&gt;<br/>
+ * </code>
+ * </p>
  */
 public class ToolListener implements ServletContextListener
 {
@@ -49,18 +58,30 @@ public class ToolListener implements ServletContextListener
 	private static Log M_log = LogFactory.getLog(ToolListener.class);
 
 	/**
+	 * The content parameter in your web.xml specifiying the webapp root relative
+	 * path to look in for the tool registration files.
+	 */
+	public final static String PATH = ToolListener.class.getName()+".PATH";
+	
+	/**
 	 * Initialize.
 	 */
 	public void contextInitialized(ServletContextEvent event)
 	{
-		// The the location of resource and registration files.
-		Set paths = event.getServletContext().getResourcePaths("/tools/");
-		if (paths == null) return;
-
-		//	First Pass:  Search for tool registration file.
-		for (Iterator i = paths.iterator(); i.hasNext();)
+		ServletContext context = event.getServletContext();
+		String toolFolder = getToolsFolder(context);
+		Set<String> paths = context.getResourcePaths(toolFolder);
+		if (paths == null)
 		{
-			String path = (String) i.next();
+			// Warn if the listener is setup but no tools found.
+			M_log.warn("No tools folder found: "+ context.getRealPath(toolFolder));
+			return;
+		}
+		int registered = 0;
+		// First Pass: Search for tool registration files
+		for(Iterator<String> i = paths.iterator(); i.hasNext();)
+		{
+			String path = i.next();
 
 			// skip directories
 			if (path.endsWith("/")) continue;
@@ -70,6 +91,7 @@ public class ToolListener implements ServletContextListener
 			{
 				M_log.info("registering tools from resource: " + path);
 				ActiveToolManager.register(event.getServletContext().getResourceAsStream(path), event.getServletContext());
+				registered++;
 			}
 		}
 
@@ -101,6 +123,11 @@ public class ToolListener implements ServletContextListener
 				}
 			}
 		}
+		if (registered == 0)
+		{
+			// Probably misconfigured as we should have at least one registered.
+			M_log.warn("No tools found to be registered.");
+	}
 	}
 
 	/**
@@ -108,5 +135,26 @@ public class ToolListener implements ServletContextListener
 	 */
 	public void contextDestroyed(ServletContextEvent event)
 	{
+	}
+	
+	protected String getToolsFolder(ServletContext context)
+	{
+		String path = context.getInitParameter(PATH);
+		if (path == null)
+		{
+			path = "/tools/";
+		}
+		else
+		{
+			if (!path.startsWith("/"))
+			{
+				path = "/"+ path;
+			}
+			if (!path.endsWith("/"))
+			{
+				path = path+ "/";
+			}
+		}
+		return path;
 	}
 }
