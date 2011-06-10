20d034d920302e9c7708a44982b0f1ec3597b452
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2011-06-10 10:23:54 +0100

    WL-1648 Prevent recursive lookups when attempting to load the entity associated with a reference. Todo this we use a threadlocal to store the previously looked up refs.

--- kernel-impl/src/main/java/org/sakaiproject/authz/impl/TwoFactorAuthenticationImpl.java
+++ kernel-impl/src/main/java/org/sakaiproject/authz/impl/TwoFactorAuthenticationImpl.java
@@ -1,5 +1,8 @@
 package org.sakaiproject.authz.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.sakaiproject.authz.api.TwoFactorAuthentication;
@@ -9,6 +12,7 @@ import org.sakaiproject.entity.api.Reference;
 import org.sakaiproject.exception.IdUnusedException;
 import org.sakaiproject.site.api.Site;
 import org.sakaiproject.site.api.SiteService;
+import org.sakaiproject.thread_local.api.ThreadLocalManager;
 import org.sakaiproject.tool.api.Session;
 import org.sakaiproject.tool.api.SessionManager;
 
@@ -25,6 +29,8 @@ public class TwoFactorAuthenticationImpl implements TwoFactorAuthentication {
 	private EntityManager entityManager;
 	
 	private SiteService siteService;
+	
+	private ThreadLocalManager threadLocalManager;
 	
 	private String siteType;
 	
@@ -45,6 +51,10 @@ public class TwoFactorAuthenticationImpl implements TwoFactorAuthentication {
 	
 	public void setSiteService(SiteService siteService) {
 		this.siteService = siteService;
+	}
+	
+	public void setThreadLocalManager(ThreadLocalManager threadLocalManager) {
+		this.threadLocalManager = threadLocalManager;
 	}
 
 	public void init() {
@@ -79,7 +89,9 @@ public class TwoFactorAuthenticationImpl implements TwoFactorAuthentication {
 	}
 
 	public boolean isTwoFactorRequired(String ref) {
-		log.debug("isTwoFactorRequired ["+ref+"]");
+		if (log.isErrorEnabled()) {
+			log.debug("isTwoFactorRequired ["+ref+"]");
+		}
 		// This checks if two factor authentication is required based on the type of the site.
 		// For references that don't have a valid context we won't be able to work out the type
 		// of the site and so won't know if we should be requiring two factor access.
@@ -87,30 +99,46 @@ public class TwoFactorAuthenticationImpl implements TwoFactorAuthentication {
 			return false;
 		}
 		if (ref != null) {
-			Reference reference = entityManager.newReference(ref);
-			if (SiteService.APPLICATION_ID.equals(reference.getType())) {
-				Object entity = reference.getEntity();
-				if (entity instanceof Site) {
-					Site site = (Site)entity;
-					if (siteType.equals(site.getType())) {
-						log.debug("isTwoFactorRequired [true]");
-						return true;
+			// Prevent stack overflow, when an unlock happens from inside here:
+			List<String> checked = (List<String>) threadLocalManager.get(TwoFactorAuthenticationImpl.class.getName());
+			if (checked == null) {
+				checked = new ArrayList<String>();
+				threadLocalManager.set(TwoFactorAuthenticationImpl.class.getName(), checked);
+			}
+			if (checked.contains(ref)) {
+				return false; // This allows the required entity to load.
+			}
+			try {
+				checked.add(ref);
+				Reference reference = entityManager.newReference(ref);
+				if (SiteService.APPLICATION_ID.equals(reference.getType())) {
+					Object entity = reference.getEntity();
+					if (entity instanceof Site) {
+						Site site = (Site)entity;
+						if (siteType.equals(site.getType())) {
+							log.debug("isTwoFactorRequired [true]");
+							return true;
+						}
+					}
+				} else {
+					// If this is a reference for something other than a site then check for the 
+					// context and use the site.
+					String siteId = reference.getContext();
+					try {
+						Site site = siteService.getSite(siteId);
+						if (siteType.equals(site.getType())) {
+							log.debug("isTwoFactorRequired [true]");
+							return true;
+						}
+					} catch (IdUnusedException iue) {
+						if (log.isInfoEnabled()) {
+							log.info("Failed to find site: "+siteId+ " for reference: "+ ref);
+						}
 					}
 				}
-			} else {
-				// If this is a reference for something other than a site then check for the 
-				// context and use the site.
-				String siteId = reference.getContext();
-				try {
-					Site site = siteService.getSite(siteId);
-					if (siteType.equals(site.getType())) {
-						log.debug("isTwoFactorRequired [true]");
-						return true;
-					}
-				} catch (IdUnusedException iue) {
-					if (log.isInfoEnabled()) {
-						log.info("Failed to find site: "+siteId+ " for reference: "+ ref);
-					}
+			} finally {
+				if(!checked.isEmpty()) {
+					checked.remove(checked.size()-1);
 				}
 			}
 		}
