420ef60eabc7a3740aeff834ebed8fef2b859542
Colin Hebert <colin.hebert@it.ox.ac.uk>
2012-11-22 16:34:26 +0000

    Revert "WL-2530 Allow loading of log4j.properties from sakai.home"
    This reverts commit 2b3bf13e7cabddc9a967e772afa0877d9137b7ac, reversing
    changes made to 0d0f635881cdaa4143c1b9ac503a3a6ca35dec40.

--- kernel-impl/src/main/java/org/sakaiproject/log/impl/Log4jConfigurationManager.java
+++ kernel-impl/src/main/java/org/sakaiproject/log/impl/Log4jConfigurationManager.java
@@ -21,7 +21,6 @@
 
 package org.sakaiproject.log.impl;
 
-import java.io.File;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -29,7 +28,10 @@ import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.*;
+import org.apache.log4j.Appender;
+import org.apache.log4j.Layout;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.log4j.spi.ErrorHandler;
 import org.apache.log4j.spi.Filter;
 import org.apache.log4j.spi.LoggingEvent;
@@ -100,11 +102,6 @@ public abstract class Log4jConfigurationManager implements LogConfigurationManag
 	{
 		if (m_enabled)
 		{
-			//Load sakai's log4j.properties file
-			String log4jConfigFilePath = serverConfigurationService().getSakaiHomePath() + "log4j.properties";
-			if(new File(log4jConfigFilePath).exists())
-				PropertyConfigurator.configure(log4jConfigFilePath);
-
 			// slip in our appender
 			Appender a = Logger.getRootLogger().getAppender("Sakai");
 			if (a != null)
