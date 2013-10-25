3c6684510ca48e3211410ec16e26c1023920a9c9
Matthew Buckett <buckett@bumph.org>
2013-10-25 08:33:25 -0700

    Merge pull request #19 from ox-it/WL-2499
    WL-2499 Support role based access to content hosting.
    Conflicts:
    	api/src/main/java/org/sakaiproject/content/api/GroupAwareEdit.java
    	kernel-impl/src/main/java/org/sakaiproject/authz/impl/BaseAuthzGroupService.java
    	kernel-impl/src/main/java/org/sakaiproject/content/impl/BaseContentService.java
    	kernel-impl/src/test/java/org/sakaiproject/content/impl/test/MockContentEntity.java
    
    Merge pull request #27 from ox-it/WL-3190
    
    WL-2499 - Merge into
    WL-3190 Refine inheritance rules for content
    
    Merge pull request #28 from ox-it/WL-3191
    
    WL-3191 Read permissions are detected correctly.
    Conflicts:
    	kernel-impl/src/main/java/org/sakaiproject/content/impl/BaseContentService.java

--- api/src/main/java/org/sakaiproject/content/api/ContentHostingService.java
+++ api/src/main/java/org/sakaiproject/content/api/ContentHostingService.java
@@ -31,6 +31,7 @@ import java.util.Stack;
 import java.util.TreeSet;
 
 import org.sakaiproject.antivirus.api.VirusFoundException;
+import org.sakaiproject.authz.api.AuthzPermissionException;
 import org.sakaiproject.entity.api.Entity;
 import org.sakaiproject.entity.api.EntityProducer;
 import org.sakaiproject.entity.api.ResourceProperties;
@@ -235,7 +236,7 @@ public interface ContentHostingService extends EntityProducer
 
 	static final String ID_LENGTH_EXCEPTION = "id_length_exception";
 
-	/**
+    /**
     * For a given id, return its UUID (creating it if it does not already exist)
     */
    public String getUuid(String id);
@@ -1607,7 +1608,8 @@ public interface ContentHostingService extends EntityProducer
 	public boolean isInheritingPubView(String id);
 
 	/**
-	 * Set this resource or collection to the pubview setting.
+	 * Set this resource or collection to the pubview setting at a low level and should be used with caution.
+	 * Recommend using org.sakaiproject.content.api.GroupAwareEdit#setPublicAccess() and #clearPublicAccess() instead.
 	 * 
 	 * @param id
 	 *        The resource or collection id.
@@ -1615,6 +1617,47 @@ public interface ContentHostingService extends EntityProducer
 	 *        The desired public view setting.
 	 */
 	public void setPubView(String id, boolean pubview);
+
+	/**
+	 * Grants or removes access to the content for a given role at a low level and should be used with caution.
+	 * Recommend using org.sakaiproject.content.api.GroupAwareEdit#addRoleAccess and #removeRoleAccess instead.
+	 *
+	 * @param id
+	 *        The resource or collection id.
+	 * @param roleId
+	 *        The id of the role to add or remove.
+	 * @param grantAccess
+	 *        The desired access setting - true gives access and false removes access.
+	 */
+	public void setRoleView(String id, String roleId, boolean grantAccess) throws AuthzPermissionException;
+
+	/**
+	 * Checks whether the given role is defined for the content.
+	 *
+	 * @param id
+	 *        The resource or collection id.
+	 * @param roleId
+	 *        The id of the role to check for.
+	 */
+	public boolean isRoleView(String id, String roleId);
+
+	/**
+	 * Checks whether the role is defined for the container of the specified entity.
+	 *
+	 * @param id
+	 *        The resource or collection id.
+	 * @param roleId
+	 *        The id of the role to check for.
+	 */
+	public boolean isInheritingRoleView(String id, String roleId);
+
+	/**
+	 * Gets a list of roles that are defined against this entity.
+	 *
+	 * @param id
+	 *        The resource or collection id.
+	 */
+	public Set<String> getRoleViews(String id);
 
 	/**
 	 * Find all resources in specified sites that match the spcified type and mime type
