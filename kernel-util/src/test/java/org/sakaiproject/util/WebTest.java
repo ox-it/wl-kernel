adbc6d34901b4c4fce7be9b05cb7948050d18853
Ben Holmes <ben.holmes@it.ox.ac.uk>
2013-11-13 13:24:53 +0000

    WL-2194 Fix encoding of ISO-8859-1 strings

--- kernel-util/src/test/java/org/sakaiproject/util/WebTest.java
+++ kernel-util/src/test/java/org/sakaiproject/util/WebTest.java
@@ -67,4 +67,9 @@ public class WebTest extends TestCase {
 				"filename*=UTF-8''%CE%93%CE%B5%CE%B9%CE%B1%20%CF%83%CE%B1%CF%82%20%CE%BA%CF%8C%CF%83%CE%BC%CE%BF.txt",
 				Web.buildContentDisposition("\u0393\u03B5\u03B9\u03B1 \u03C3\u03B1\u03C2 \u03BA\u03CC\u03C3\u03BC\u03BF.txt", false));
 	}
+
+	public void testContentDispositionISO8859() {
+		assertEquals("inline; filename=\"exposé.txt\"; filename*=UTF-8''expos%C3%A9.txt",
+				Web.buildContentDisposition("exposé.txt", false));
+	}
 }
