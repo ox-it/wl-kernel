3d59016778680a8da93b845ebcd35b41f6c3e438
Matthew Buckett <buckett@bumph.org>
2013-11-13 08:39:55 -0800

    Merge pull request #24 from ox-it/WL-2194
    WL-2194 Encode UTF-8 in Content Disposition correctly
    Conflicts:
    	kernel-util/src/main/java/org/sakaiproject/util/Web.java
    	kernel-util/src/test/java/org/sakaiproject/util/WebTest.java

--- kernel-impl/src/main/java/org/sakaiproject/content/impl/BaseContentService.java
+++ kernel-impl/src/main/java/org/sakaiproject/content/impl/BaseContentService.java
@@ -6756,8 +6756,7 @@ SiteContentAdvisorProvider, SiteContentAdvisorTypeRegistry, EntityTransferrerRef
 			else
 			{
 				// use the last part, the file name part of the id, for the download file name
-				String fileName = Web.encodeFileName( req, Validator.getFileName(ref.getId()) );
-
+				String fileName = Validator.getFileName(ref.getId());
 				String disposition = null;
 
 				if (Validator.letBrowserInline(contentType))
@@ -6790,17 +6789,17 @@ SiteContentAdvisorProvider, SiteContentAdvisorTypeRegistry, EntityTransferrerRef
 						}		
 						
 						if (fileInline || folderInline) {
-							disposition = "inline; filename=\"" + fileName + "\"";
+							disposition = Web.buildContentDisposition(fileName, false);
 						}
 					} else {
-						disposition = "inline; filename=\"" + fileName + "\"";
+						disposition = Web.buildContentDisposition(fileName, false);
 					}
 				}
 				
 				// drop through to attachment
 				if (disposition == null)
 				{
-					disposition = "attachment; filename=\"" + fileName + "\"";
+					disposition = Web.buildContentDisposition(fileName, true);
 				}
 
 				// NOTE: Only set the encoding on the content we have to.
